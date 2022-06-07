c2a5830996c5812745e71859ec9e76b07f99bdab
Nicholas Ochs <nick.ochs@yahoo.com>
2022-06-07 16:58:42 -0400

    fixes share icons and adds admin route button

--- components/layout/footer.component.tsx
+++ components/layout/footer.component.tsx
@@ -1,30 +1,40 @@
-import Link from 'next/link';
 import React from 'react';
-import { FaFacebookSquare, FaInstagram, FaTiktok } from 'react-icons/fa';
-import { BsTelephone } from 'react-icons/bs';
+import Link from 'next/link';
+
 import { AiOutlineMail } from 'react-icons/ai';
+import { BsTelephone } from 'react-icons/bs';
+import { FaFacebookSquare, FaInstagram, FaTiktok } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
 	return (
 		<footer className='w-screen bg-primaryBrown p-4'>
-			<div className='space-y-4'>
-				<div className='flex items-center justify-center space-x-8'>
-					<Link href='https://www.facebook.com/Finna-Eat-109601537220056/' className='rounded-md p-2 hover:bg-footerButtonHover'>
-						<a>
-							<FaFacebookSquare size='35px' color='#C41D33' />
-						</a>
-					</Link>
-					<Link href='https://www.instagram.com/finnaeatfoods/' className='rounded-md p-2 hover:bg-footerButtonHover'>
-						<a>
-							<FaInstagram size='35px' color='#C41D33' />
-						</a>
-					</Link>
-					<Link href='https://www.tiktok.com/@finnaeatllc?' className='rounded-md p-2 hover:bg-footerButtonHover'>
-						<a>
-							<FaTiktok size='35px' color='#C41D33' />
-						</a>
-					</Link>
+			<div className='space-y-3'>
+				{/* Social Links */}
+				<div className='flex items-center justify-center space-x-4'>
+					<div className='rounded-md p-2 hover:bg-footerButtonHover'>
+						<Link href='https://www.facebook.com/Finna-Eat-109601537220056/'>
+							<a>
+								<FaFacebookSquare size='35px' color='#C41D33' />
+							</a>
+						</Link>
+					</div>
+					<div className='rounded-md p-2 hover:bg-footerButtonHover'>
+						<Link href='https://www.instagram.com/finnaeatfoods/' className='rounded-md p-2 hover:bg-footerButtonHover'>
+							<a>
+								<FaInstagram size='35px' color='#C41D33' />
+							</a>
+						</Link>
+					</div>
+					<div className='rounded-md p-2 hover:bg-footerButtonHover'>
+						<Link href='https://www.tiktok.com/@finnaeatllc?' className='rounded-md p-2 hover:bg-footerButtonHover'>
+							<a>
+								<FaTiktok size='35px' color='#C41D33' />
+							</a>
+						</Link>
+					</div>
 				</div>
+
+				{/* Contact Information */}
 				<div className='flex flex-col items-center justify-center'>
 					<a className='flex items-center text-white' href='tel:504-215-3686'>
 						<BsTelephone className='mr-2' size='18px' />
@@ -34,6 +44,13 @@ const Footer: React.FC = () => {
 						<AiOutlineMail className='mr-2' size='18px' />
 						finnaeatfoods@gmail.com
 					</a>
+				</div>
+
+				{/* Admin Route */}
+				<div className='flex items-center justify-center'>
+					<Link href='#'>
+						<a className='btn-primary rounded-2xl bg-primaryRed px-4 py-1 normal-case text-white'>Admin</a>
+					</Link>
 				</div>
 			</div>
 		</footer>
